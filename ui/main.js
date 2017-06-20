cbeb62609690689e148479335d66521a1084b58c
karthik1905 <karthiksince1998@gmail.com>
2017-06-21 04:31:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,13 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = "new value";
-
-//move the image
-var img = document.getElementById("madi");
-img.onclick = function()
-{
-    img.style.marginleft = '100px';
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick=function() {
+ // make req to counter endppint
+ 
+ //capture the response and store it in a vaiable
+ 
+ //render the variable in correct span
+ counter = counter +1;
+ var span= document.getElementById('count');
+ span.innerHTML=counter.toString();    
 };
