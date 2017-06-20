c8bf994acaa5b0cde46f316dce23fdc9f84d7b03
karthik1905 <karthiksince1998@gmail.com>
2017-06-21 04:48:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,21 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick=function() {
  // make req to counter endppint
+  var request = new XMLHTTPRequet();
  
  //capture the response and store it in a vaiable
+  request.onreadystatechange =  function() {
+      if(request.readyState === XMLHTTPRequest.DONE) {
+          //take some action
+          if(request.status == 200) {
+              var counter = request .responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
+  };
  
- //render the variable in correct span
- counter = counter +1;
- var span= document.getElementById('count');
- span.innerHTML=counter.toString();    
+ //make request 
+ request.open('GET', 'http://karthik1905.imad.hasura-app.io/counter',true);
+ request.send(null);
 };
