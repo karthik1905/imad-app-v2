4eacdb02f430c3d9d948bf21a434e8a56f9a77b9
karthik1905 <karthiksince1998@gmail.com>
2017-06-21 05:13:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,5 +20,18 @@ button.onclick=function() {
  //make request 
  request.open('GET', 'http://karthik1905.imad.hasura-app.io/counter',true);
  request.send(null);
- 
 };
+ //submit none
+ var nameINput= document.getElementById('name');
+ var name = nameInput.value;
+ var submit = document.getElementById('submit-btn');
+ submit.onclick = function() {
+   var names =['name1' , ' name2' , 'name3' , 'name4'];
+   var list =' ';
+   for (var i=0; i<names.length ; i++)
+   {
+       list+='<li>' + names[i] + '<li>';
+   }
+   var ul = document.getElementById('namelist');
+   ul.innerHTML = list;
+ };
